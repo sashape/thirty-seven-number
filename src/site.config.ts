498910bbe1613961cd507a80f14e0bdd57f3a298
Alex Penskoy <apien@list.ru>
2024-08-03 15:42:47 +0300

    add design, add general pages

---
+++ src/site.config.ts
@@ -0,0 +1,9 @@
+import type { SeoTags } from '@/types';
+export const defaultSeoTags: SeoTags = {
+	author: "Tom Magliery",
+	description: "Site about number 37",
+	lang: "en-US",
+	title: "37 Heaven",
+  favicon: '/favicon.svg',
+  ogImage: ''
+};
---
+++ types/SeoTags.ts
@@ -0,0 +1,8 @@
+export interface SeoTags {
+	author: string;
+	description: string;
+	lang: string;
+	title: string;
+	favicon: string;
+	ogImage?: string | undefined;
+}
